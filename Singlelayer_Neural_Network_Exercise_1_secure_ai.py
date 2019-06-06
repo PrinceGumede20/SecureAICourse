00cd545e0776a3ed0c20cc35b78a34494d40f388
PrinceGumede20 <webstermandla01@gmail.com>
2019-06-06 16:50:28 +0200

    Update Singlelayer_Neural_Network_Exercise_1_secure_ai.py

--- Singlelayer_Neural_Network_Exercise_1_secure_ai.py
+++ Singlelayer_Neural_Network_Exercise_1_secure_ai.py
@@ -31,3 +31,28 @@ mytensor2 = weights.shape
 newweights =weights.view(5 , 1)
 myTensor = activation(torch.mm(features, weights.view(5 , 1)) + bias)
 print(myTensor)
+
+#Calculate the multilayer Network
+
+torch.manual_seed(7) # Set the random seed so things are predictable
+
+# Features are 3 random normal variables
+features = torch.randn((1, 3))
+
+# Define the size of each layer in our network
+n_input = features.shape[1]     
+n_hidden = 2                     
+n_output = 1                    
+
+# Weights for inputs to hidden layer
+W1 = torch.randn(n_input, n_hidden)
+# Weights for hidden layer to output layer
+W2 = torch.randn(n_hidden, n_output)
+
+# and bias terms for hidden and output layers
+B1 = torch.randn((1, n_hidden))
+B2 = torch.randn((1, n_output))
+
+h = activation(torch.mm(features, W1) + B1)
+output = activation(torch.mm(h, W2) + B2)
+print(output)
